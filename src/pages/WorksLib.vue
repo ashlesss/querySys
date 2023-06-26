afcd54c1db93c47cee2b6eb0d747317bb1f4939e
ashlesss <hidahza@gmail.com>
2023-06-26 02:05:46 -0400

    support pagination with 3 works per page

--- src/pages/WorksLib.vue
+++ src/pages/WorksLib.vue
@@ -1,17 +1,19 @@
 <template>
-    <q-infinite-scroll @load="requestWorks" :offset="250">
+    <!-- <q-infinite-scroll @load="requestWorks" :offset="250"> -->
         <div class="row q-px-sm q-col-gutter-x-md q-col-gutter-y-lg">
             <!-- col-xs-12 col-sm-4 col-md-3 col-lg-2 col-xl-2 -->
             <div class="col-xs-12 col-sm-4 col-md-3 col-lg-2 col-xl-2" v-for="work in works" :key="work.rjcode">
                 <WordCard :work="work" class="fit" /> 
             </div>
         </div>
-    </q-infinite-scroll>
+    <!-- </q-infinite-scroll> -->
     <div class="q-pa-lg flex flex-center">
         <q-pagination
-        v-model="current"
-        :max="5"
+        v-model="currPage"
+        :max="maxPage"
+        :max-pages="6"
         direction-links
+        @update:model-value="pageChange($event)"
         />
     </div>
 </template>
@@ -35,18 +37,33 @@ export default defineComponent({
                 // {rjcode: 'RJ440172'},
                 // {rjcode: 'RJ440172'},
             ],
-            current: 1,
+            currPage: 1,
             pagination: {currentPage: 1, pageSize: 3, totalCount: 0},
+            maxPage: 10,
         }
     },
+    // created() {
+    //     console.log(this.currPage);
+    // },
 
     methods: {
+        pageChange(pageNumber) {
+            this.currPage = pageNumber
+            // console.log(pageNumber);
+            this.requestWorks()
+        },
+
         requestWorks() {
-            const url = `/api/query/rc`
+            this.works= []
+            const url = `/api/query/rc?page=${this.currPage}`
             this.$axios.get(url)
             .then(val => {
                 // console.log(val.data[0].work[0].rj_code);
-                for ( let i = 0; i < val.data.length; i++) {
+                // console.log(val.data[val.data.length - 1].current_page);
+                this.maxPage = val.data[val.data.length - 2].max_page
+                this.currPage = val.data[val.data.length - 1].current_page
+                // console.log(val.data[val.data.length - 1].max_page);
+                for ( let i = 0; i < val.data.length - 2; i++) {
                     // console.log(val.data[i].work[0].rj_code);
                     this.works.push({rj_code: val.data[i].work[0].rj_code, 
                         work_main_img: val.data[i].work[0].work_main_img,
@@ -54,7 +71,11 @@ export default defineComponent({
                         work_tags: val.data[i].tags});
                 }
             })
-        }
+        },
+    },
+
+    mounted() {
+        this.requestWorks()
     }
 })
 </script>
